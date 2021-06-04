456a01dc11fb027456dcfbce3dcd302e391f24c2
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2021-06-04 15:58:39 +0300

    Upgrade 'devextreme' to 21.1
    https://devexpress.com/issue=T985868

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 
-import * as devextremeAjax from 'devextreme/core/utils/ajax.js';
+import devextremeAjax from 'devextreme/core/utils/ajax';
 import { sendRequestFactory } from '../ng-http-client-helper';
 
 import { AppComponent } from './app.component';
